fab17359b80665ca17d962c6fe35846e6f9e945d
CalebUAz <calebshibu@email.arizona.edu>
2022-11-22 23:04:46 -0700

    batch size dropped to 50

--- code/TrojanNet/trojannet.py
+++ code/TrojanNet/trojannet.py
@@ -27,7 +27,7 @@ class TrojanNet:
         self.shape = (8, 8)
         self.attack_left_up_point = (150, 150)
         self.epochs = 1000
-        self.batch_size = 250
+        self.batch_size = 50
         self.random_size = 200
         self.training_step = None
         pass
